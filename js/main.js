71df9a86bc6a745c953a35a175c2f12d361c3b4a
hrhalim <hrhalimbd8624@gmail.com>
2023-03-03 13:18:31 +0600

    Modal style done

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
     Fetch Universe Hub Using Funtions
 */
 const fetchUniverseHub = () =>{
-    const URL = `https://openapi.programming-hero.com/api/ai/tools`;
+    const URL = `https://openapi.programming-hero.com/api/ai/tools`; 
     fetch(URL)
     .then(res => res.json())
     .then(data => displayUniverseHub(data.data.tools))
 }
+
 
 /*
 
@@ -15,23 +16,39 @@ const fetchUniverseHub = () =>{
 
 */
 
+
 const displayUniverseHub = (universHub) =>{
-  const unverseWrapper = document.getElementById('universe-wrappr');
-   
-  universHub = universHub.slice(0, 6);
+  const unverseWrapper = document.getElementById('universe-wrappr');  
+ const seeMore = document.getElementById('see-all'); 
+//  if(universHub.length > 6) {
+//     seeMore.classList.remove('d-none');
+//     console.log('work');
+//     universHub = universHub.slice(0, 6); 
+//  }else {
+//     seeMore.classList.add('d-none');
+//     console.log('not work');
+//  }
+
+  
+
   universHub.forEach(universe => {
-    console.log(universe);
-    const div = document.createElement('div');
+    // console.log(universe);
+    const div = document.createElement('div'); 
     div.classList.add('col'); 
-    const {image, features, name, published_in} = universe;
-    
+    const {image, features, name, published_in, id} = universe;  
     div.innerHTML = ` 
     <div class="card p-3">
     <img src="${image}" class="card-img-top rounded-4" alt="...">
     <div class="card-body p-0">
       <h5 class="card-title pt-3">Features</h5> 
-      <ol id="features-list"> 
-     ${features}
+      <ol id="features-list">  
+      ${features
+        .map(
+        (feature, index) => `
+        ${index + 1}. ${feature}
+        `
+        )
+        .join(" <br> ")}
       </ol>
       <div class="card-bottom d-flex justify-content-between border-top pt-4 mt-4">
         <div class="ai-left">
@@ -39,7 +56,7 @@ const displayUniverseHub = (universHub) =>{
             <h6 class="d-flex gap-2"><img src="images/date.png" alt="date"><span>${published_in}</span></h6>
         </div>
         <div class="ai-right">
-        <button type="button" class="border border-0 p-3 rounded-circle bg-light" data-bs-toggle="modal" data-bs-target="#universeModal">
+        <button type="button" onclick="fetchUniverseDetails('${id}')" class="border border-0 p-3 rounded-circle bg-light" data-bs-toggle="modal" data-bs-target="#universeModal">
         <img src="images/arrow-icon.png" alt="Arrow">
         </button> 
         </div>
@@ -49,6 +66,9 @@ const displayUniverseHub = (universHub) =>{
   
   `
   unverseWrapper.appendChild(div);
+  
+    
+
   });
   
 }
@@ -60,6 +80,69 @@ const displayUniverseHub = (universHub) =>{
  
 */
 
+const fetchUniverseDetails = (universeId) =>{
+    const URL = `https://openapi.programming-hero.com/api/ai/tool/${universeId}`; 
+    console.log(URL);
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => displayUniverseDetails(data.data)) 
+}
+
+/* 
+
+    Display Universe Hub Details  
+
+*/
+
+const displayUniverseDetails = (singleData) =>{  
+
+    console.log(singleData);
+
+    // Object distructuring
+    const {description, pricing, input_output_examples, image_link, accuracy, features, integrations} = singleData; 
+    
+    const descriptionTitle = document.getElementById('universeModalLabel');
+    descriptionTitle.innerHTML = description;
+    const univerImage = document.getElementById('universe-img');
+    univerImage.innerHTML =` <img class="w-100" src="${image_link[0]}"> `
+    const exampleTitle = document.getElementById('example-title');
+    exampleTitle.innerHTML = input_output_examples[0].input;
+    const exampleContent = document.getElementById('example-content');
+    exampleContent.innerHTML = input_output_examples[0].output ? input_output_examples[0].output : 'No! Not Yet! Take a break!!!';
+    const btnAccuracy = document.getElementById('btn-accuracy');
+    btnAccuracy.innerHTML = accuracy.score ? accuracy.score : "";
+    const basicPrice = document.getElementById('basic-price'); 
+    basicPrice.innerHTML = pricing[0].price ? pricing[0].price : 'Free Of Cost/Basic';
+    const proPrice = document.getElementById('pro-price');
+    proPrice.innerHTML =  pricing[1].price ? pricing[1].price : 'Free Of Cost/Pro';
+    const enterPrice = document.getElementById('enterprice');
+    enterPrice.innerHTML = pricing[2].price ? pricing[2].price : 'Free of Cost /Enterprise';
+    
+
+    //Get Features List
+    const featuresList = document.getElementById('single-features-wrapper');
+    featuresList.innerHTML = ""; 
+    const FeaturesValue = Object.values(features); 
+    FeaturesValue.map(item =>{
+        const li = document.createElement('li');
+        li.innerHTML = item.feature_name ? item.feature_name : 'No data Found';
+        featuresList.appendChild(li); 
+    }) 
+
+
+    //Get Integrations list
+    const integrationsList = document.getElementById('intergration-list');
+    integrationsList.innerHTML = "";   
+    integrations.map(listItem =>{
+        const li = document.createElement('li');
+        li.innerHTML = listItem ? listItem : 'No data Found';
+        integrationsList.appendChild(li); 
+    }) 
+
+
+
+}
+
 
 /*
 
@@ -67,5 +150,10 @@ const displayUniverseHub = (universHub) =>{
 
 */
 
+document.getElementById('btn-seemore').addEventListener('click', function(){
+    fetchUniverseHub();
+});
 
 fetchUniverseHub();
+
+
